859b5514523da42392da4025f86a8a2dfbc59db3
naanatin <naanatin@gmail.com>
2021-10-09 20:20:25 +0300

    anekdootit step2+3

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 
+const Header = (props) => <h1>{props.text}</h1>
+
 const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
 
-// satunnaislukugeneraattori, joka arpoo satunnaisen kokonaisluvun väliltä min...max-1.
-// Math.random() ei koskaan palauta maksimiarvoa (exclusive), joten maksimin tulee olla
-// yhtä kokonaislukua suurempi kuin todellinen haluttu maksimi, tässä tapauksessa 7 ( = anekdoottitaulukon koko).
-const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max-min) + min)
+const Display = (props) => {
+  return (
+  <>
+  <p>{props.text}</p>
+  <p>has {props.points} points.</p>
+  </>
+  )
+}
 
 const App = () => {
   const anecdotes = [
@@ -19,14 +25,46 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
-
   const min = 0
   const max = anecdotes.length
+  const [points] = useState(() => new Array(max).fill(0))
+  let [top, setTop] = useState(0)
+  console.log(points)
+  
+  // satunnaislukugeneraattori, joka arpoo satunnaisen kokonaisluvun väliltä min...max-1.
+  // Math.random() ei koskaan palauta maksimiarvoa (exclusive), joten maksimin tulee olla
+  // yhtä kokonaislukua suurempi kuin todellinen haluttu maksimi, tässä tapauksessa 7 ( = anekdoottitaulukon koko).
+  const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max-min) + min)
+
+  const updatePoints = (index) => {
+      const copy = [...points]
+      copy[index] += 1
+      points[index] = copy[index]
+      setTop(updateTop())
+      console.log(points)
+      console.log(top)
+  }
+
+  const updateTop = () => {
+    // selvitetään äänestyksen yhteydessä korkein pistemäärä, ja tallennetaan sen anekdootin indeksi
+    // top-muuttujaan. Oletuksena näkyy ensimmäinen anekdootti.
+    for (let i=0; i<7; i++) {
+      console.log(i)
+      if (points[i] > points[top]) {
+        top = i
+      }
+    }
+    return top
+  }
 
   return (
     <div>
+      <Header text='Anecdote of the day' />
       <p>{anecdotes[selected]}</p>
+      <Button handleClick={() => updatePoints(selected)} text ='Vote' />
       <Button handleClick={() => setSelected(generateRandomNumber(min, max))} text='Next anecdote' />
+      <Header text='Anecdote with most votes' />
+      <Display text={anecdotes[top]} points={points[top]} />
     </div>
   )
 }
